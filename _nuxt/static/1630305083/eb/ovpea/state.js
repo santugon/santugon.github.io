50a3677f6120a2003cffcfc04d74393daf7f1fb9
Manolo Enriquez <noloenriquez@gmail.com>
2021-08-30 14:31:51 +0800

    spoa updates

---
+++ _nuxt/static/1630305083/eb/ovpea/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1630305083",layout:"default",error:a,serverRendered:true,routePath:"\u002Feb\u002Fovpea",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null));
