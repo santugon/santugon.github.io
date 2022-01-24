131a8dd620f899d97419e64e3558e1b06c42be44
Manolo Enriquez <noloenriquez@gmail.com>
2022-01-24 18:28:34 +0800

    fixed safari video issues

--- _next/static/enWhSyjoKVBv4GPKpnfoz/_ssgManifest.js
+++
@@ -1,1 +0,0 @@
-self.__SSG_MANIFEST=new Set(["\u002Fla","\u002F[college]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
---
+++ _next/static/gSwm8w1eEu_FSlwiN0XVf/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002Fla","\u002F[college]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
